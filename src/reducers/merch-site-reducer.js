45ae16d486b5c706d3f31ba839954ef6c31ca1da
Nick Lindau <nicholaithegreat@gmail.com>
2021-04-26 13:43:40 -0700

    pass 2nd test Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>

--- src/reducers/merch-site-reducer.js
+++ src/reducers/merch-site-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, description, quantity, fullStock, id } = action;
+  switch (action.type) {
+    case "ADD_ITEM":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          description: description,
+          quantity: quantity,
+          fullStock: fullStock,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 }
